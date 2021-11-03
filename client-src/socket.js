0ce19f7c4a0bb096e1fe33748106e926df0e1b9d
Isao Jonas <isao.jonas@gmail.com>
2021-11-03 20:29:36 +0000

    fix: fix client socket retry logic

--- client-src/socket.js
+++ client-src/socket.js
@@ -15,50 +15,45 @@ const Client =
     : WebSocketClient;
 /* eslint-enable camelcase */
 
-let retries = 0;
-let maxRetries = 10;
-let client = null;
+const initSocket = function initSocket(url, handlers, reconnect) {
+  let retries = 0;
+  const maxRetries = reconnect;
 
-const socket = function initSocket(url, handlers, reconnect) {
-  client = new Client(url);
+  function socket() {
+    const client = new Client(url);
 
-  client.onOpen(() => {
-    retries = 0;
-    maxRetries = reconnect;
-  });
+    client.onClose(() => {
+      if (retries === 0) {
+        handlers.close();
+      }
 
-  client.onClose(() => {
-    if (retries === 0) {
-      handlers.close();
-    }
+      // After maxRetries, stop trying in order to prevent logspam.
+      if (retries < maxRetries) {
+        // Exponentially increase timeout to reconnect.
+        // Respectfully copied from the package `got`.
+        // eslint-disable-next-line no-mixed-operators, no-restricted-properties
+        const retryInMs = 1000 * Math.pow(2, retries) + Math.random() * 100;
 
-    // Try to reconnect.
-    client = null;
+        retries += 1;
 
-    // After 10 retries stop trying, to prevent logspam.
-    if (retries < maxRetries) {
-      // Exponentially increase timeout to reconnect.
-      // Respectfully copied from the package `got`.
-      // eslint-disable-next-line no-mixed-operators, no-restricted-properties
-      const retryInMs = 1000 * Math.pow(2, retries) + Math.random() * 100;
+        log.info("Trying to reconnect...");
 
-      retries += 1;
+        setTimeout(() => {
+          socket();
+        }, retryInMs);
+      }
+    });
 
-      log.info("Trying to reconnect...");
+    client.onMessage((data) => {
+      const message = JSON.parse(data);
 
-      setTimeout(() => {
-        socket(url, handlers);
-      }, retryInMs);
-    }
-  });
+      if (handlers[message.type]) {
+        handlers[message.type](message.data);
+      }
+    });
+  }
 
-  client.onMessage((data) => {
-    const message = JSON.parse(data);
-
-    if (handlers[message.type]) {
-      handlers[message.type](message.data);
-    }
-  });
+  socket();
 };
 
-export default socket;
+export default initSocket;
